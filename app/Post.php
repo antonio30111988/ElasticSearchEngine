ab85b47c4a835c0cbb2c8a79b8817c0fb6cacaa7
Antonio <antonio.conipo@gmail.com>
2017-04-12 18:04:13 +0200

    Implemented 3rd party Elastic library for Laravel

--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
+use Elasticquent\ElasticquentTrait;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    public $fillable=['title','content','tags'];
+    use ElasticquentTrait;
+	
+	public $fillable=['title','content','tags'];
+	
 }
